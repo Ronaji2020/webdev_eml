7255c3df5d921023a115d6736b6595062fcf7851
Roberto Nava <ronaji@yahoo.com>
2022-04-26 10:20:44 -0500

    Init 64

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  
+<div>
+  <h3>Control de Flujo Masico</h3>
+    <button @click="temperatura--">Disminuir</button>
+    <span>{{temperatura}}</span><span> °C</span>
+    <button @click="temperatura++">Aumentar</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        temperatura: 10,
+        humedad: 0,
+      }
+    }
+  }
+</script>
